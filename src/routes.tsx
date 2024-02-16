0f43029b9f9184c11b820def8d5c26b2c85b4a59
Victor Hugo <v.perricci@grouplinknetwork.com>
2024-02-16 14:44:05 -0300

    adicionando rota de sign-up

--- src/routes.tsx
+++ src/routes.tsx
@@ -3,6 +3,7 @@ import { createBrowserRouter } from 'react-router-dom'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
+import { SignUp } from './pages/auth/sign-up'
 import { SignIn } from './pages/auth/sing-in'
 
 export const router = createBrowserRouter([
@@ -14,6 +15,9 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AuthLayout />,
-    children: [{ path: '/sign-in', element: <SignIn /> }],
+    children: [
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
+    ],
   },
 ])
